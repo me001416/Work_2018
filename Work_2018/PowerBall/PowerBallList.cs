fa9301920f008395b70171f224b71b3909f0a804
me001416 <me001416@gmail.com>
2018-02-22 16:02:35 +0800

    Version 1.0.21

---
+++ Work_2018/PowerBall/PowerBallList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Work_2018.PowerBall
+{
+    class PowerBallList
+    {
+
+    }
+
+    class PowerBall
+    {
+        //
+        // List of number.
+        //
+        public List<int> NumList { get; private set; }
+
+        public int Mouth { get; private set; }
+        public int Day { get; private set; }
+        public int Year { get; private set; }
+    }
+}
