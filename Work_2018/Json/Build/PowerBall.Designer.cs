0f3a72dd6afee9f525be250b796ab5a4b5841091
me001416 <me001416@gmail.com>
2018-02-22 11:28:58 +0800

    Version 1.0.4

--- Work_2018/Json/Build/PowerBall.Designer.cs
+++ Work_2018/Json/Build/PowerBall.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -37,23 +38,34 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "選取";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 64);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(257, 22);
+            this.textBox1.TabIndex = 1;
             // 
             // PowerBall
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(310, 118);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "PowerBall";
             this.Text = "PowerBall";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
--- Work_2018/Json/Build/PowerBall.cs
+++ Work_2018/Json/Build/PowerBall.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,24 @@ namespace Work_2018.Json.Build
         public PowerBall()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog();
+            ofg.InitialDirectory = Properties.Settings.Default.PowerBallDataPath;
+            ofg.Multiselect = false;
+            ofg.ShowDialog();
+            textBox1.Text = ofg.FileName;
+
+            if (string.IsNullOrEmpty(ofg.FileName))
+            {
+                return;
+            }
+
+            FileInfo fi = new FileInfo(ofg.FileName);
+            Properties.Settings.Default.PowerBallDataPath = fi.DirectoryName;
+            Properties.Settings.Default.Save();
         }
     }
 }
