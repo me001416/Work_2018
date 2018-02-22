b7b82f7c79fae72ba48753cd4515bb07b88f5c02
me001416 <me001416@gmail.com>
2018-02-22 11:18:53 +0800

    Version 1.0.1

---
+++ Work_2018/Json/Build/PowerBall.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace Work_2018.Json.Build
+{
+    partial class PowerBall
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // PowerBall
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 118);
+            this.Name = "PowerBall";
+            this.Text = "PowerBall";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Work_2018/Json/Build/PowerBall.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work_2018.Json.Build
+{
+    public partial class PowerBall : Form
+    {
+        public PowerBall()
+        {
+            InitializeComponent();
+        }
+    }
+}
