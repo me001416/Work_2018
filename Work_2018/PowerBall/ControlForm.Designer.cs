50ed3dbb22e304345fd987e3af87cf0fc101358e
me001416 <me001416@gmail.com>
2018-02-22 15:56:19 +0800

    Version 1.0.20

--- Work_2018/PowerBall/ControlForm.Designer.cs
+++ Work_2018/PowerBall/ControlForm.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace Work_2018
+﻿namespace Work_2018.PowerBall
 {
-    partial class Form1
+    partial class ControlForm
     {
         /// <summary>
-        /// 設計工具所需的變數。
+        /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// 清除任何使用中的資源。
+        /// Clean up any resources being used.
         /// </summary>
-        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,15 +20,16 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form 設計工具產生的程式碼
+        #region Windows Form Designer generated code
 
         /// <summary>
-        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
-        /// 這個方法的內容。
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -37,25 +38,34 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "SourceFile";
+            this.button1.Text = "開啟";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // Form1
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(228, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // ControlForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 143);
+            this.ClientSize = new System.Drawing.Size(341, 144);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "ControlForm";
+            this.Text = "ControlForm";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
-
---
+++ Work_2018/PowerBall/ControlForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Work_2018.PowerBall
+{
+    public partial class ControlForm : Form
+    {
+        string FileName;
+
+        public ControlForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofg = new OpenFileDialog();
+            ofg.InitialDirectory = Properties.Settings.Default.PowerBallSavePath;
+            ofg.Multiselect = false;
+            ofg.ShowDialog();
+            FileName = ofg.FileName;
+            textBox1.Text = ofg.FileName;
+
+            if (string.IsNullOrEmpty(ofg.FileName))
+            {
+                return;
+            }
+
+            FileInfo mFileInfo = new FileInfo(ofg.FileName);
+            Properties.Settings.Default.PowerBallSavePath = mFileInfo.DirectoryName;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
