d25ac3be8cd7690bc0f4599a5e66b917cf7dc0b0
me001416 <me001416@gmail.com>
2018-07-19 18:26:31 +0800

    Version 1.0.31

---
+++ Work_2018/PowerBall/Subsets/Subsets.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace PowerBall
+{
+    /// <summary>
+    /// Subsets of combination.
+    /// </summary>
+    public class Subsets
+    {
+        public string Name { get; private set; }
+        public int Count { get; private set; }
+        public List<int> numList { get; private set; }
+
+        /// <summary>
+        /// 建構函數
+        /// </summary>
+        /// <param name="SrcNumList">資料來源</param>
+        public Subsets(List<int> SrcNumList, int SrcCount)
+        {
+            numList = SrcNumList;
+            Name = BuildName(numList);
+            Count = SrcCount;
+        }
+
+        private string BuildName(List<int> SrcNumList)
+        {
+            string str = string.Empty;
+
+            if (SrcNumList.Count == 0)
+            {
+                Debug.Assert(false, "Subsets.cs", "srcNumList.Count == 0");
+            }
+
+            for (int i = 0; i < SrcNumList.Count; i++)
+            {
+                str = str + "[" + SrcNumList[i].ToString() + "]";
+            }
+
+            return str;
+        }
+
+        /// <summary>
+        /// 長度
+        /// </summary>
+        public int Length()
+        {
+            return numList.Count;
+        }
+    } /// public class Subsets
+} /// namespace PowerBall
---
+++ Work_2018/PowerBall/Subsets/SubsetsList.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace PowerBall
+{
+    public class SubsetsList
+    {
+        public List<Subsets> mSubsetsList { get; private set; }
+        //public List<IntervalRecord> mIntervalRecordList { get; private set; }
+
+        /// <summary>
+        /// 建構函數
+        /// </summary>
+        public SubsetsList()
+        {
+            mSubsetsList = new List<Subsets>();
+        }
+
+        /// <summary>
+        /// mSubsetsList 的數量
+        /// </summary>
+        public int Count()
+        {
+            return mSubsetsList.Count;
+        }
+
+        /// <summary>
+        /// 資料數量，Ex:[1][2][3] = 3
+        /// </summary>
+        public int FirstLength()
+        {
+            return mSubsetsList[0].Length();
+        }
+
+        /// <summary>
+        /// 將 class Subsets 加入 mSubsetsList
+        /// </summary>
+        public void Add(Subsets SrcSubsets)
+        {
+            mSubsetsList.Add(SrcSubsets);
+        }
+    } /// public class SubsetsList
+} /// namespace PowerBall
