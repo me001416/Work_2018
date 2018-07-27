368c12c70b18b4da918632edecb3ff239f80f8fe
me001416 <me001416@gmail.com>
2018-07-27 14:45:38 +0800

    Version 1.0.34

--- Work_2018/Combinations/Generate.cs
+++ Work_2018/Combinations/Generate.cs
@@ -27,6 +27,25 @@ namespace Combination
         }
 
         /// <summary>
+        /// 產生所有 Combination 並回傳
+        /// </summary>
+        /// <param name="SrcList">資料來源</param>
+        /// <param name="k">深度，x 個數字中取 k </param>
+        /// <returns>回傳所有 Combination</returns>
+        public List<List<byte>> NewCombine(List<byte> SrcList, int k)
+        {
+            List<List<byte>> mResultList = new List<List<byte>>();
+
+            if (SrcList.Count != 0 && k != 0 && SrcList.Count >= k)
+            {
+                List<byte> mTempList = new List<byte>();
+                doCombin(0, SrcList.Count, k, mResultList, mTempList, SrcList);
+            }
+
+            return mResultList;
+        }
+
+        /// <summary>
         /// 計算 Combination
         /// </summary>
         /// <param name="start">起始次數</param>
@@ -49,6 +68,39 @@ namespace Combination
                 if (deep - 1 == 0)
                 {
                     mResultList.Add(new List<int>(mTempList));
+                    mTempList.RemoveAt(mTempList.Count - 1);
+                }
+                else
+                {
+                    doCombin(i + 1, end, deep - 1, mResultList, mTempList, mSrcList);
+                    mTempList.RemoveAt(mTempList.Count - 1);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 計算 Combination
+        /// </summary>
+        /// <param name="start">起始次數</param>
+        /// <param name="end">最大次數</param>
+        /// <param name="deep">深度</param>
+        /// <param name="mResultList">結果</param>
+        /// <param name="mTempList">暫存用</param>
+        /// <param name="mSrcList">資料來源</param>
+        public void doCombin(int start, int end, int deep, List<List<byte>> mResultList, List<byte> mTempList, List<byte> mSrcList)
+        {
+            for (int i = start; i < end; i++)
+            {
+                if (end - i + 1 < deep)
+                {
+                    return;
+                }
+
+                mTempList.Add(mSrcList[i]);
+
+                if (deep - 1 == 0)
+                {
+                    mResultList.Add(new List<byte>(mTempList));
                     mTempList.RemoveAt(mTempList.Count - 1);
                 }
                 else
--- Work_2018/PowerBall/PowerBallList.cs
+++ Work_2018/PowerBall/PowerBallList.cs
@@ -25,7 +25,7 @@ namespace PowerBall
 
         public void Dump()
         {
-#if DEBUG
+#if DEBUGN
             PList.ForEach(x =>
             {
                 Trace.WriteLine(DateTime.Now);
